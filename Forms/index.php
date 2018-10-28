2e8cdb9dcbad9118b6c1a38c7ae23a637214b014
hreeves19 <hreeves@islander.tamucc.edu>
2018-10-28 12:47:34 -0500

    Adding more files

---
+++ Forms/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Courtland
+ * Date: 10/28/2018
+ * Time: 12:46 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
